6a6163d687cb138007114752c3160b9e05c2ad21
David Becker <david@beckersite.com>
2017-09-22 16:49:20 -0400

    add navheader links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router';
+import NavHeader from './NavHeader/NavHeader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route exact path='/' component={NavHeader} />
         test
       </div>
     );
---
+++ src/NavHeader/NavHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavHeader.css';
+import { Link } from 'react-router-dom';
+
+const NavHeader = () => {
+	return (
+		<header>
+      <Link to='/'>Home</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/about-me'>About Me</Link>
+      <Link to='/blog'>Blog</Link>
+    </header>
+	);
+};
+
+export default NavHeader;
