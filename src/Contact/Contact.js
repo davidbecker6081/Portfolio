47d50bc6973e6bbf347250a6e38cb7865da2c703
David Becker <david@beckersite.com>
2017-10-08 16:01:11 -0600

    Add subject input

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -7,6 +7,7 @@ const Contact = () => {
 			<h2>Contact Me</h2>
 			<form method="POST" action="http://formspree.io/david@beckersite.com">
 				<input type="email" name="email" placeholder="Your email" />
+				<input type="text" name="_subject" placeholder="Subject" />
 				<textarea name="message" placeholder="Your message" />
 				<button type="submit">Send</button>
 			</form>
